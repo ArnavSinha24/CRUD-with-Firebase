c853e9169d277a2a16f924adc7bc4eb09731f0b2
Arnav Sinha <121484127+ArnavSinha24@users.noreply.github.com>
2024-08-05 19:11:02 +0530

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCf03Nzn3mamu8soHp80U8pHg8hn7vpHFA",
+  authDomain: "myfireprowithcrud-5dde3.firebaseapp.com",
+  databaseURL: "https://myfireprowithcrud-5dde3-default-rtdb.firebaseio.com",
+  projectId: "myfireprowithcrud-5dde3",
+  storageBucket: "myfireprowithcrud-5dde3.appspot.com",
+  messagingSenderId: "338752459023",
+  appId: "1:338752459023:web:cdaa0d7872439cadd8ae7a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
